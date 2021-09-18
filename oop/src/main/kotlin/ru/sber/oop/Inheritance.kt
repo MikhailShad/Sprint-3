9e0df8d327e3cdf8f82c90564658abe64c4a00aa
Шадрин Михаил Дмитриевич <shadrin.m.dm@sberbank.ru>
2021-09-18 13:36:20 +0300

    added solution for Inheritance.kt

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,13 +1,19 @@
 package ru.sber.oop
 
-class Room(val name: String, val size: Int) {
+open class Room(val name: String, val size: Int) {
+    protected open val dangerLevel = 5
 
-    val dangerLevel = 5
+    constructor(name: String) : this(name, 100)
 
     fun description() = "Room: $name"
 
-    fun load() = "Nothing much to see here..."
+    open fun load() = "Nothing much to see here..."
 
 }
 
-//TODO: create class TownSquare here...
+class TownSquare() : Room("Town Square", 1000) {
+    final override fun load() = "Loading..."
+
+    override val dangerLevel = super.dangerLevel - 3
+}
+

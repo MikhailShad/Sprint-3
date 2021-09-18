9204755efbaeac40d9de3f9970c8193c00e8e6d9
Шадрин Михаил Дмитриевич <shadrin.m.dm@sberbank.ru>
2021-09-18 14:05:07 +0300

    added solution for Polymorphism.kt

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,18 +1,21 @@
 package ru.sber.oop
 
+fun Monster.getSalutation() = "I'll kill you"
+
 open class Room(val name: String, val size: Int) {
     protected open val dangerLevel = 5
+    private val monster: Monster = Goblin("Goblusha", "Cute goblin", "Weak attack", 100)
 
     constructor(name: String) : this(name, 100)
 
     fun description() = "Room: $name"
 
-    open fun load() = "Nothing much to see here..."
+    open fun load() = "Nothing much to see here... Except for the Monster:\"${monster.getSalutation()}\"!"
 
 }
 
 class TownSquare() : Room("Town Square", 1000) {
-    final override fun load() = "Loading..."
+    final override fun load() = "Loading $name"
 
     override val dangerLevel = super.dangerLevel - 3
 }
